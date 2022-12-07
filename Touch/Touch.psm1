a5541d2ae01b671baa90808b6cf63239a5cbe557
Olai <94807067+OlaYZen@users.noreply.github.com>
2022-12-07 10:42:48 +0100

    Update Touch.psm1

--- Touch/Touch.psm1
+++ Touch/Touch.psm1
@@ -13,13 +13,6 @@ function touch
     }
     else
     {
-        # UTF-16 LE
-        # echo $null > $file
-
-        # UTF-8 With BOM
-        # $null | Out-File $file -Encoding "UTF8"
-        
-        # UTF-8 W/O BOM
         [IO.File]::WriteAllLines("$pwd\$file", "")
     }
 }
